8f88272177891d2860823a52dbe3d1d357e30803
tusharchemate <tushar.devidas@qapita.com>
2024-06-02 13:17:40 +0530

    create execution context for greeter

---
+++ Greeter.js
@@ -0,0 +1,18 @@
+(function (global, $) {
+  var Greeter = function (firstName, lastName, language) {
+    return new Greeter.init(firstName, lastName, language);
+  };
+
+  Greeter.prototype = {};
+
+  Greeter.init = function (firstName, lastName, language) {
+    var self = this;
+    (self.firstName = firstName || ""),
+      (self.lastName = lastName || ""),
+      (self.language = language || "en");
+  };
+
+  Greeter.init.prototype = Greeter.prototype;
+
+  global.Greeter = global.G$ = Greeter;
+})(window, jQuery);
